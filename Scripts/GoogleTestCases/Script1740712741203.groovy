e457f905cda3e4624deddfd2c1232fa52bdea7f9
Sakthi Madhu <151857561+sakthimadhu07@users.noreply.github.com>
2025-03-17 06:12:21 +0530

    Update Script1740712741203.groovy

--- Scripts/GoogleTestCases/Script1740712741203.groovy
+++ Scripts/GoogleTestCases/Script1740712741203.groovy
@@ -6,6 +6,7 @@ import internal.GlobalVariable as GlobalVariable
 WebUI.openBrowser("https://www.google.co.in/")
 
 WebUI.maximizeWindow()
+WebUI.refresh()
 
 //WebUI.setText(findTestObject('Object Repository/Google/SearchBar'), Name)
 
